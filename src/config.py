797b56c236d369849c2251e115c7dff049bf62d5
gowtham-0010 <sgowtham1009gmail.com>
2025-10-28 22:49:30 +0530

    Day 1 complete: Project setup, MySQL connection, environment configured

---
+++ src/config.py
@@ -0,0 +1,17 @@
+﻿# src/config.py
+
+
+
+# MySQL Database Configuration
+DB_CONFIG = { 
+    'host': 'localhost',
+    'user': 'root',                 # <-- use same as Workbench
+    'password': 'gowtham@0010',  # <-- replace with ACTUAL password 
+    'database': 'amazon_sales',     # <-- will create this next
+    'port': 3306
+}
+
+# File Paths
+RAW_DATA_PATH = 'data/raw/'
+PROCESSED_DATA_PATH = 'data/processed/'
+INTERIM_DATA_PATH = 'data/interim/'
---
+++ src/database/db_connection.py
@@ -0,0 +1,30 @@
+﻿import pymysql
+from src.config import DB_CONFIG
+
+def create_database():
+    connection = pymysql.connect(
+        host=DB_CONFIG['host'],
+        user=DB_CONFIG['user'],
+        password=DB_CONFIG['password'],
+        port=DB_CONFIG['port']
+    )
+    cursor = connection.cursor()
+    cursor.execute("CREATE DATABASE IF NOT EXISTS amazon_sales;")
+    connection.commit()
+    cursor.close()
+    connection.close()
+    print("✅ Database 'amazon_sales' is ready!")
+
+def test_connection():
+    try:
+        connection = pymysql.connect(
+            **DB_CONFIG
+        )
+        print("✅ Successfully connected to MySQL database!")
+        connection.close()
+    except Exception as e:
+        print("❌ Error:", e)
+
+if __name__ == "__main__":
+    create_database()
+    test_connection()
